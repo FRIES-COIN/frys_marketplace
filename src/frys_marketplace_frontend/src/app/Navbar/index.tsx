86f5067a0b1d83a90b97c92fb3512c7eb0f978fc
divin3circle <omutelema.silas21@students.dkut.ac.ke>
2024-10-16 11:37:07 +0300

    Refactor Navbar component for improved structure and styling

--- src/frys_marketplace_frontend/src/app/Navbar/index.tsx
+++ src/frys_marketplace_frontend/src/app/Navbar/index.tsx
@@ -1,10 +1,9 @@
-import React from "react";
-import Image from "../../public/logo.png";
 import { FaRocket, FaUser } from "react-icons/fa";
+import avatar from "../../../public/avatar1.svg";
 
 function Navbar() {
   return (
-    <nav className="bg-[#0C0C0C]  flex items-center justify-between px-4 py-2">
+    <nav className="bg-[#0C0C0C]  flex items-center justify-between px-4 py-2 font-body">
       <div className="flex items-center">
         <img
           src="../../logo.png"
@@ -13,7 +12,7 @@ function Navbar() {
           height={56}
           className="mr-2"
         />
-        <span className="text-[#ffa503] font-['Ravi_Prakash'] text-[24.58px] leading-[56px] font-normal font-[400]">
+        <span className="text-[#ffa503] font-body text-[24.58px] leading-[56px] font-bold">
           FRYS
         </span>
       </div>
@@ -21,13 +20,13 @@ function Navbar() {
       <div className="flex items-center space-x-8">
         <a
           href="#"
-          className="text-white font-semibold font-['Rubik'] text-[20.96px] leading-[24.84px] w-[131px] h-[25px] hover:text-[#ffa503] hover:text-[22.96px] transition-all duration-500"
+          className="text-white font-semibold font-body text-[20.96px] leading-[24.84px] w-[131px] h-[25px] hover:text-[#ffa503] hover:text-[22.96px] transition-all duration-500"
         >
           COLLECTION
         </a>
         <a
           href="#"
-          className="text-white font-semibold font-['Rubik'] text-[20.96px] leading-[24.84px] w-[131px] h-[25px] hover:text-[#ffa503] hover:text-[22.96px] transition-all duration-500"
+          className="text-white font-semibold font-body text-[20.96px] leading-[24.84px] w-[131px] h-[25px] hover:text-[#ffa503] hover:text-[22.96px] transition-all duration-500"
         >
           MINT
         </a>
@@ -35,9 +34,15 @@ function Navbar() {
 
       <div className="flex items-center space-x-4">
         <div className="w-10 h-10 rounded-full bg-gray-300 flex items-center justify-center">
-          <img src="../../avater.png" alt="Profile" width={40} height={40} />
+          <img
+            src={avatar}
+            alt="Profile"
+            width={40}
+            height={40}
+            className="rounded-full"
+          />
         </div>
-        <button className="bg-transparent border-[4.66px] border-[#ffa503] rounded-[17.47px] text-white px-4 py-2 flex items-center hover:bg-[#ffa503] hover:text-black transition-all duration-500">
+        <button className="bg-transparent border-[2.66px] border-[#ffa503] font-body rounded-[17.47px] text-white px-4 py-2 flex items-center hover:bg-[#ffa503] hover:text-black transition-all duration-500">
           <span className="mr-2">COOK</span>
           <FaRocket />
         </button>
