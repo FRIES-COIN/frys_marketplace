95cd0933eef052536a990973c688d1b0ee6ea08c
divin3circle <omutelema.silas21@students.dkut.ac.ke>
2024-10-24 23:23:29 +0300

    Refactor package.json to add dependencies for react-hot-toast and react-router-dom

---
+++ src/frys_marketplace_frontend/pages/Collection.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Collection() {
+  return (
+    <div>
+      <h1>Collection page</h1>
+    </div>
+  );
+}
+
+export default Collection;
---
+++ src/frys_marketplace_frontend/pages/Mint.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Mint() {
+  return (
+    <div>
+      <h1>Mint page</h1>
+    </div>
+  );
+}
+
+export default Mint;
