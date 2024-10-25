54f38898fc3e5a5d9df0541aff749df08639fc54
divin3circle <omutelema.silas21@students.dkut.ac.ke>
2024-10-25 07:19:22 +0300

    feat: Refactor Mint page to include Navbar and MintForm components

--- src/frys_marketplace_frontend/pages/Mint.tsx
+++ src/frys_marketplace_frontend/pages/Mint.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import Navbar from "../src/app/Navbar";
+import MintPage from "../src/app/mint";
 
 function Mint() {
   return (
     <div>
-      <h1>Mint page</h1>
+      <Navbar />
+      <section className="lg:px-12 md:px-4">
+        <MintPage />
+      </section>
     </div>
   );
 }
