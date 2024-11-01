06d7b18f49507d2dd5e398537cce6b4c0b0fae73
divin3circle <omutelema.silas21@students.dkut.ac.ke>
2024-11-01 21:59:49 +0300

    Refactor package.json dependencies and add new UI components

--- src/frys_marketplace_frontend/src/app/profile/sidebar.tsx
+++ src/frys_marketplace_frontend/src/app/profile/sidebar.tsx
@@ -6,10 +6,25 @@ import {
   IconUserFilled,
   IconWallet,
 } from "@tabler/icons-react";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "../../../components/ui/select";
+import { Input } from "../../../components/ui/input";
 import { CarouselSize } from "./nfts-owned";
 import { useState } from "react";
 type Tab = "nft" | "wallet" | "settings" | "profile" | "logout";
+import btc from "../../../public/btc.svg";
+import icp from "../../../public/icp.svg";
+import eth from "../../../public/eth.svg";
+import frys from "../../../public/frys.jpeg";
+import qr from "../../../public/qr.png";
+import { Button } from "../../../components/ui/button";
 
+//NFT TAB COMPONENT
 function NFTTab() {
   return (
     <div className="bg-primary rounded-md h-full">
@@ -22,6 +37,102 @@ function NFTTab() {
         </button>
       </div>
       <CarouselSize />
+    </div>
+  );
+}
+
+//WALLET TAB COMPONENT
+function WalletTab() {
+  return (
+    <div className="bg-[#151415] rounded-md h-full">
+      <div className="text-white font-body w-full pt-4 flex items-center justify-center">
+        <Select>
+          <SelectTrigger className="w-1/4 border-none bg-[#202020] h-16 -mr-2">
+            <SelectValue
+              placeholder="Choose token"
+              className="flex items-center gap-2 placeholder:text-gray-500"
+            />
+          </SelectTrigger>
+          <SelectContent className="bg-[#202020] w-[14rem] border-none">
+            <SelectItem
+              value="btc"
+              className="flex items-center gap-2 justify-between w-full mt-4"
+            >
+              <div className="w-full flex flex-row items-center gap-2">
+                <img src={btc} alt="btc" className="w-8 h-8" />
+                <p className="uppercase font-body font-semibold text-gray-500">
+                  btc
+                </p>
+              </div>
+            </SelectItem>
+            <SelectItem
+              value="icp"
+              className="flex items-center gap-2 justify-between w-full mt-4"
+            >
+              <div className="w-full flex flex-row items-center gap-2">
+                <img src={icp} alt="icp" className="w-8 h-8" />
+                <p className="uppercase font-body font-semibold text-gray-500">
+                  icp
+                </p>
+              </div>
+            </SelectItem>
+            <SelectItem
+              value="eth"
+              className="flex items-center gap-2 justify-between w-full mt-4"
+            >
+              <div className="w-full flex flex-row items-center gap-2">
+                <img src={eth} alt="eth" className="w-8 h-8" />
+                <p className="uppercase font-body font-semibold text-gray-500">
+                  eth
+                </p>
+              </div>
+            </SelectItem>
+            <SelectItem
+              value="frys"
+              className="flex items-center gap-2 justify-between w-full mt-4"
+            >
+              <div className="w-full flex flex-row items-center gap-2">
+                <img
+                  src={frys}
+                  alt="FRYS COIN"
+                  className="w-8 h-8 rounded-full"
+                />
+                <p className="uppercase font-body font-semibold text-gray-500">
+                  frys
+                </p>
+              </div>
+            </SelectItem>
+          </SelectContent>
+        </Select>
+        <Input
+          placeholder="0.0000"
+          className="w-1/4 border-none bg-[#202020] h-16 focus:ring-0 focus:border-0 focus:outline-none"
+        />
+      </div>
+      <div>
+        <img src={qr} className="w-[20%] mx-auto mt-8 rounded-md" />
+      </div>
+      <div className="w-full flex items-center justify-center mt-4">
+        <Input
+          placeholder="Enter deposit address"
+          className="w-1/2 border-none bg-[#202020] text-white h-16 focus:ring-0 focus:border-0 focus:outline-none"
+        />
+      </div>
+      <div className="flex items-center justify-center mt-4">
+        <p className="text-center font-body text-xs leading-relaxed text-gray-500 w-1/2">
+          Always start with small amounts of transactions whether you are newbie
+          or an expert and be on the look out for phishing scams going on in the
+          crypto industry.
+        </p>
+      </div>
+      <div className="flex items-center justify-between">
+        <Button className="bg-primary text-white w-1/4 mx-auto mt-8">
+          Send
+        </Button>
+        <Button className="bg-primary text-white w-1/4 mx-auto mt-8">
+          Receive
+        </Button>
+      </div>
     </div>
   );
 }
@@ -66,7 +177,9 @@ function Sidebar() {
             onClick={() => setTab("profile")}
           >
             <IconUserFilled size={24} className="text-white" />
-            <h1 className="text-white font-body">Profile</h1>
+            <h1 className="text-white font-body transition-all duration-150">
+              Profile
+            </h1>
           </div>
         </section>
         <section className="flex flex-col gap-8">
@@ -81,7 +194,7 @@ function Sidebar() {
         </section>
       </nav>
       <aside className="flex-1 h-3/4">
-        <NFTTab />
+        <WalletTab />
       </aside>
     </div>
   );
