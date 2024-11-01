79eea3e12561b2b400e77e7a889a5bab2abe6265
divin3circle <omutelema.silas21@students.dkut.ac.ke>
2024-11-01 12:36:32 +0300

    Refactor error page layout and add back home link

--- src/frys_marketplace_frontend/pages/Error.tsx
+++ src/frys_marketplace_frontend/pages/Error.tsx
@@ -5,9 +5,16 @@ import Footer from "../src/app/Footer";
 function Error() {
   return (
     <div className=" px-4 flex items-center justify-center flex-col">
+      <img
+        src="https://img.freepik.com/free-vector/404-error-with-landscape-concept-illustration_114360-7898.jpg"
+        className="mt-12 rounded-xl"
+      />
       <h1 className="uppercase tracking-widest text-gray-500">
         404 | Not Found
       </h1>
+      <Link to="/" className=" text-white underline mt-8">
+        Back Home
+      </Link>
     </div>
   );
 }
